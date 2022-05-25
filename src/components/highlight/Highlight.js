193b14b872cac3f2c725e219a72c227eecd3626f
stevensotelo <stevenbetancurt@hotmail.com>
2022-05-24 20:50:21 -0500

    updated database

--- src/components/highlight/Highlight.js
+++ src/components/highlight/Highlight.js
@@ -3,6 +3,15 @@ import KeyData from '../key_data/KeyData';
 import KeySection from '../key_section/KeySection';
 
 function Highlight(props) {
+    const calculate_space = (amount) => {
+        return amount == 4
+            ? "3"
+            : amount == 3
+                ? "4"
+                : amount == 2
+                    ? "6" : "1"
+            ;
+    }
     return (
         <>
             {props.data ? props.data.map((item, index) => (
@@ -12,7 +21,7 @@ function Highlight(props) {
                     </div>
                     <div className='row'>
                         {item.key_data.map((item2, index2) => (
-                            <KeyData icon={item2.icon} content={item2.content} value={item2.value} space={"4"} />
+                            <KeyData icon={item2.icon} content={item2.content} value={item2.value} space={calculate_space(item.key_data.length)} />
                         )) ??
                             <></>
                         }
